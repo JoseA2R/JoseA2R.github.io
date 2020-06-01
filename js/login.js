451a7825f3df9b0c59fae5863005557fee37626b
diogo20eng <diogojmferreira@hotmail.com>
2020-06-01 20:35:31 +0100

    avaliacao

--- js/login.js
+++ js/login.js
@@ -421,7 +421,7 @@ function isLoggedOn() {
 //redireciona para a pagina de login caso nao tenha feito ainda
 function redirectLogin() {
   if (!isLoggedOn()) {
-    window.location.replace("login.html");
+    window.location.replace("index.html");
   }
 }
 
