48cb8db141ad1de923b9abc8966e622dfd6f7c12
diogo20eng <diogojmferreira@hotmail.com>
2020-06-04 20:00:22 +0100

    ultimo

--- js/login.js
+++ js/login.js
@@ -50,6 +50,7 @@ function getNum(s) {
 //retorna um objeto representante de uma reserva individual
 function newReservaInd() {
   var reserva = {
+    botao:  '',
     tipo: '',
     user: '',
     sala: '',
@@ -57,22 +58,6 @@ function newReservaInd() {
     hora_init: '',
     hora_end: '',
     confirmada: ''
-  }
-  return reserva;
-}
-
-//retorna um objeto representante de uma reserva de grupo
-function newReservaGrupo() {
-  var reserva = {
-    id: '',
-    tipo: '',
-    user: '', //aluno que realiza a reserva
-    sala: '',
-    data: '',
-    hora_init: '',
-    hora_end: '',
-    confirmada: '',
-    lista_confirmacoes: []  //lista dos colegas: [{numero, confirmada}, ...]
   }
   return reserva;
 }
@@ -95,13 +80,6 @@ function saveListaReservasInd(lista) {
   //console.log(JSON.parse(localStorage.getItem('listaReservasInd')));
 }
 
-//guarda a lista de reservas de grupo
-function saveListaReservasGrupo(lista) {
-  var listaReservasGrupo = JSON.stringify(lista);
-  localStorage.setItem('listaReservasGrupo', listaReservasGrupo);
-  //console.log(JSON.parse(localStorage.getItem('listaReservasGrupo')));
-}
-
 //guarda a lista das notificações
 function saveListaNotifications(lista) {
   var listaNotifications = JSON.stringify(lista);
@@ -112,17 +90,6 @@ function saveListaNotifications(lista) {
 //carrega a lista de reservas individuais
 function loadListaReservasInd() {
   var lista = localStorage.getItem('listaReservasInd');
-  if (lista == null) {
-    return [];
-  }
-  else {
-    return JSON.parse(lista);
-  }
-}
-
-//carrega a lista de reservas de groupo
-function loadListaReservasGrupo() {
-  var lista = localStorage.getItem('listaReservasGrupo');
   if (lista == null) {
     return [];
   }
@@ -165,6 +132,18 @@ function getHorario() {
       hora_end: "13:00"
     },
     {
+      dia: "Monday",
+      disciplina: "RCD",
+      hora_init: "14:00",
+      hora_end: "16:00"
+    },
+    {
+      dia: "Monday",
+      disciplina: "TLC",
+      hora_init: "16:00",
+      hora_end: "18:00"
+    },
+    {
       dia: "Tuesday",
       disciplina: "HCI",
       hora_init: "09:00",
@@ -177,20 +156,8 @@ function getHorario() {
       hora_end: "13:00"
     },
     {
-      dia: "Tuesday",
-      disciplina: "ER",
-      hora_init: "14:00",
-      hora_end: "16:00"
-    },
-    {
-      dia: "Tuesday",
-      disciplina: "RCD",
-      hora_init: "18:00",
-      hora_end: "20:00"
-    },
-    {
       dia: "Wednesday",
-      disciplina: "TLC",
+      disciplina: "ER",
       hora_init: "09:00",
       hora_end: "11:00"
     },
@@ -223,20 +190,6 @@ function intersetaHorario(hora_init, hora_end, data) {
   }
 
   //console.log("nao interseta");
-  return false;
-}
-
-//verifica se uma reserva interseta com o fecho de uma sala
-function intersetaFechoSala(reserva, fecho) {
-  if (reserva.sala === fecho.sala && reserva.data === fecho.data) {
-    if (reserva.hora_init >= fecho.hora_init && reserva.hora_init < fecho.hora_end) {
-      return true;
-    }
-    if (reserva.hora_end > fecho.hora_init && reserva.hora_end <= fecho.hora_end) {
-      return true;
-    }
-  }
-
   return false;
 }
 
@@ -326,22 +279,6 @@ function getNotifications(num) {
     }
   }
   return lista_final;
-}
-
-//verifica se uma reserva de grupo ja recebeu todas as confirmações
-function todasConfirmadas(id) {
-  var listaReservasGrupo = loadListaReservasGrupo();
-  for (var i = 0; i < listaReservasGrupo.length; i++) {
-    if (listaReservasGrupo[i].id == id) {
-      for (var j = 0; j < listaReservasGrupo[i].lista_confirmacoes.length; j++) {
-        if (listaReservasGrupo[i].lista_confirmacoes[j].confirmado === false) {
-          return false;
-        }
-      }
-      break;
-    }
-  }
-  return true;
 }
 
 //apos todas as confirmações, é eliminada a reserva de grupo
@@ -471,3 +408,178 @@ function todosDiferentes(arr, val) {
   }
   return true;
 }
+
+
+
+//audio
+
+//webkitURL is deprecated but nevertheless
+URL = window.URL || window.webkitURL;
+
+var gumStream; 						//stream from getUserMedia()
+var rec; 							//Recorder.js object
+var input; 							//MediaStreamAudioSourceNode we'll be recording
+
+// shim for AudioContext when it's not avb. 
+var AudioContext = window.AudioContext || window.webkitAudioContext;
+var audioContext //audio context to help us record
+
+var recordButton = document.getElementById("recordButton");
+var stopButton = document.getElementById("stopButton");
+var pauseButton = document.getElementById("pauseButton");
+
+//add events to those 2 buttons
+recordButton.addEventListener("click", startRecording);
+stopButton.addEventListener("click", stopRecording);
+pauseButton.addEventListener("click", pauseRecording);
+
+function startRecording() {
+	console.log("recordButton clicked");
+
+	/*
+		Simple constraints object, for more advanced audio features see
+		https://addpipe.com/blog/audio-constraints-getusermedia/
+	*/
+    
+    var constraints = { audio: true, video:false }
+
+ 	/*
+    	Disable the record button until we get a success or fail from getUserMedia() 
+	*/
+
+	recordButton.disabled = true;
+	stopButton.disabled = false;
+	pauseButton.disabled = false
+
+	/*
+    	We're using the standard promise based getUserMedia() 
+    	https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
+	*/
+
+	navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+		console.log("getUserMedia() success, stream created, initializing Recorder.js ...");
+
+		/*
+			create an audio context after getUserMedia is called
+			sampleRate might change after getUserMedia is called, like it does on macOS when recording through AirPods
+			the sampleRate defaults to the one set in your OS for your playback device
+		*/
+		audioContext = new AudioContext();
+
+		//update the format 
+		document.getElementById("formats").innerHTML="Format: 1 channel pcm @ "+audioContext.sampleRate/1000+"kHz"
+
+		/*  assign to gumStream for later use  */
+		gumStream = stream;
+		
+		/* use the stream */
+		input = audioContext.createMediaStreamSource(stream);
+
+		/* 
+			Create the Recorder object and configure to record mono sound (1 channel)
+			Recording 2 channels  will double the file size
+		*/
+		rec = new Recorder(input,{numChannels:1})
+
+		//start the recording process
+		rec.record()
+
+		console.log("Recording started");
+
+	}).catch(function(err) {
+	  	//enable the record button if getUserMedia() fails
+    	recordButton.disabled = false;
+    	stopButton.disabled = true;
+    	pauseButton.disabled = true
+	});
+}
+
+function pauseRecording(){
+	console.log("pauseButton clicked rec.recording=",rec.recording );
+	if (rec.recording){
+		//pause
+		rec.stop();
+		pauseButton.innerHTML="Resume";
+	}else{
+		//resume
+		rec.record()
+		pauseButton.innerHTML="Pause";
+
+	}
+}
+
+function stopRecording() {
+	console.log("stopButton clicked");
+
+	//disable the stop button, enable the record too allow for new recordings
+	stopButton.disabled = true;
+	recordButton.disabled = false;
+	pauseButton.disabled = true;
+
+	//reset button just in case the recording is stopped while paused
+	pauseButton.innerHTML="Pause";
+	
+	//tell the recorder to stop the recording
+	rec.stop();
+
+	//stop microphone access
+	gumStream.getAudioTracks()[0].stop();
+
+	//create the wav blob and pass it on to createDownloadLink
+	rec.exportWAV(createDownloadLink);
+}
+
+function createDownloadLink(blob) {
+	
+	var url = URL.createObjectURL(blob);
+	var au = document.createElement('audio');
+	var li = document.createElement('li');
+	var link = document.createElement('a');
+
+	//name of .wav file to use during upload and download (without extendion)
+	var filename = new Date().toISOString();
+
+	//add controls to the <audio> element
+	au.controls = true;
+	au.src = url;
+
+	//save to disk link
+	link.href = url;
+	link.download = filename+".wav"; //download forces the browser to donwload the file using the  filename
+	link.innerHTML = "Save to disk";
+
+	//add the new audio element to li
+	li.appendChild(au);
+	
+	//add the filename to the li
+	li.appendChild(document.createTextNode(filename+".wav "))
+
+	//add the save to disk link to li
+	li.appendChild(link);
+	
+	//upload link
+	var upload = document.createElement('a');
+	upload.href="#";
+	upload.innerHTML = "Upload";
+	upload.addEventListener("click", function(event){
+		  var xhr=new XMLHttpRequest();
+		  xhr.onload=function(e) {
+		      if(this.readyState === 4) {
+		          console.log("Server returned: ",e.target.responseText);
+		      }
+		  };
+		  var fd=new FormData();
+		  fd.append("audio_data",blob, filename);
+		  xhr.open("POST","upload.php",true);
+		  xhr.send(fd);
+	})
+	li.appendChild(document.createTextNode (" "))//add a space in between
+	li.appendChild(upload)//add the upload link to li
+
+	//add the li element to the ol
+	recordingsList.appendChild(li);
+}
+
+
+
+//
