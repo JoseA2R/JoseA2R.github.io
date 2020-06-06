fc7b8e6b54c94ab41f4fd02767c1829919d5eed5
JoseA2R <joseagustindrf@gmail.com>
2020-06-06 17:02:12 +0100

    sendgrid working (errors encountered)

--- SendGrid-API/send-email.php
+++ SendGrid-API/send-email.php
@@ -3,25 +3,26 @@
 require_once ('vendor/autoload.php');
 
 /*Post Data*/
-$type =           $_POST['type'];
-$name =           $_POST['name'];
-$email =          $_POST['email'];
-$message =        $_POST['message'];
-$filename =       $_POST['fileName'];
+$type       =     $_POST['type'];
+$name       =     $_POST['name'];
+$email      =     $_POST['email'];
+$subject     =     $_POST['subject'];
+$message    =     $_POST['message'];
+$filename   =     $_POST['fileName'];
 $attachment =     $_POST['code'];
-die(var_dump($type));
+
 /*Content*/
-$from = new SendGrid\Email("Code", "joseagustindrf@gmail.com");
-$subject = "Testing Mail";
-$to = new SendGrid\Email("Jose", $email);
-$content1 = new SendGrid\Content("text/html", $message);
-die(var_dump($attachment));
+$from = new SendGrid\Email("CodeMent", "diogojmferreira@hotmail.com");
+$subject = $subject;
+$to = new SendGrid\Email("CodeMent", $email);
+$content1 = new SendGrid\Content("text/plain", $message);
+
 $content = new \SendGrid\Attachment();
 $content->setContent(base64_encode($attachment));
 $content->setType($type);
 $content->setFilename($filename);
 $content->setDisposition("attachment");
-//die(var_dump($content));
+
 /*Send the mail*/
 $mail = new SendGrid\Mail($from, $subject, $to, $content1);
 $mail->addAttachment( $content );
