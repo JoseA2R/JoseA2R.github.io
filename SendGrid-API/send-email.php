797242944c7b14445f965ea6a407f08fd8c6efbd
JoseA2R <joseagustindrf@gmail.com>
2020-06-06 22:48:50 +0100

    modifications

--- SendGrid-API/send-email.php
+++ SendGrid-API/send-email.php
@@ -3,14 +3,17 @@
 require_once ('vendor/autoload.php');
 
 /*Post Data*/
-$type       =     $_POST['type'];
-$name       =     $_POST['name'];
-$email      =     $_POST['email'];
-$subject     =     $_POST['subject'];
-$message    =     $_POST['message'];
-$filename   =     $_POST['fileName'];
-$attachment =     $_POST['code'];
-
+$type          = $_POST['type'];
+$name          = $_POST['name'];
+$email         = $_POST['email'];
+$subject       = $_POST['subject'];
+$message       = $_POST['message'];
+$filename      = $_POST['fileName'];
+$attachment    = $_POST['code'];
+$video         = $_POST['fileContent'];
+$videoNameFile = $_POST['nameVideo'];
+$voiceNote     = $_POST['voiceNote0'];
+//die(var_dump($video));
 /*Content*/
 $from = new SendGrid\Email("CodeMent", "diogojmferreira@hotmail.com");
 $subject = $subject;
@@ -23,19 +26,35 @@ $content->setType($type);
 $content->setFilename($filename);
 $content->setDisposition("attachment");
 
+if(isset($video) && !empty($video)){
+    $contentVideo = new \SendGrid\Attachment();
+    $contentVideo->setcontent(base64_encode(file_get_contents($video)));
+    $contentVideo->setType('video/webm');
+    $contentVideo->setFilename($filename);
+    $contentVideo->setDisposition("attachment");
+}
+
+if(isset($voiceNote) && !empty($voiceNote)){
+    $contentAudio = new \SendGrid\Attachment();
+    $contentAudio->setcontent(base64_encode($voiceNote));
+    $contentAudio->setType('audio/x-wav');
+    $contentAudio->setFilename($filename);
+    $contentAudio->setDisposition("attachment");
+}
+
 /*Send the mail*/
 $mail = new SendGrid\Mail($from, $subject, $to, $content1);
 $mail->addAttachment( $content );
+if(isset($video) && !empty($video)){
+    $mail->addAttachment( $contentVideo );
+}
 $apiKey = ('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');
 $sg = new \SendGrid($apiKey);
 
 /*Response*/
 $response = $sg->client->mail()->send()->post($mail);
+
+header('Location: ' . $_SERVER['HTTP_REFERER']);
+
 ?>
 
-<!--Print the response-->
-<pre>
-    <?php
-    var_dump($response);
-    ?>
-</pre>
--- js/login.js
+++ js/login.js
@@ -50,7 +50,7 @@ function getNum(s) {
 //retorna um objeto representante de uma reserva individual
 function newReservaInd() {
   var reserva = {
-    botao:  '',
+    botao: '',
     tipo: '',
     user: '',
     sala: '',
@@ -335,8 +335,8 @@ function todosDiferentes(arr, val) {
   //verificar se cada valor do array é diferente de todos os outros
   for (var i = 0; i < arr.length; i++) {
     for (var j = 0; j < arr.length; j++) {
-      if (i !== j){
-        if(arr[i] === arr[j]){
+      if (i !== j) {
+        if (arr[i] === arr[j]) {
           return false;
         }
       }
@@ -370,206 +370,219 @@ stopButton.addEventListener("click", stopRecording);
 pauseButton.addEventListener("click", pauseRecording);
 
 function startRecording() {
-	console.log("recordButton clicked");
+  console.log("recordButton clicked");
 
 	/*
 		Simple constraints object, for more advanced audio features see
 		https://addpipe.com/blog/audio-constraints-getusermedia/
 	*/
-    
-    var constraints = { audio: true, video:false }
 
- 	/*
-    	Disable the record button until we get a success or fail from getUserMedia() 
-	*/
+  var constraints = { audio: true, video: false }
 
-	recordButton.disabled = true;
-	stopButton.disabled = false;
-	pauseButton.disabled = false
+  /*
+    Disable the record button until we get a success or fail from getUserMedia() 
+*/
+
+  recordButton.disabled = true;
+  stopButton.disabled = false;
+  pauseButton.disabled = false
 
 	/*
     	We're using the standard promise based getUserMedia() 
     	https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
 	*/
 
-	navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
-		console.log("getUserMedia() success, stream created, initializing Recorder.js ...");
+  navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
+    console.log("getUserMedia() success, stream created, initializing Recorder.js ...");
 
 		/*
 			create an audio context after getUserMedia is called
 			sampleRate might change after getUserMedia is called, like it does on macOS when recording through AirPods
 			the sampleRate defaults to the one set in your OS for your playback device
 		*/
-		audioContext = new AudioContext();
+    audioContext = new AudioContext();
 
-		//update the format 
-		document.getElementById("formats").innerHTML="Format: 1 channel pcm @ "+audioContext.sampleRate/1000+"kHz"
+    //update the format 
+    document.getElementById("formats").innerHTML = "Formato: 1 channel pcm @ " + audioContext.sampleRate / 1000 + "kHz"
 
-		/*  assign to gumStream for later use  */
-		gumStream = stream;
-		
-		/* use the stream */
-		input = audioContext.createMediaStreamSource(stream);
+    /*  assign to gumStream for later use  */
+    gumStream = stream;
+
+    /* use the stream */
+    input = audioContext.createMediaStreamSource(stream);
 
 		/* 
 			Create the Recorder object and configure to record mono sound (1 channel)
 			Recording 2 channels  will double the file size
 		*/
-		rec = new Recorder(input,{numChannels:1})
+    rec = new Recorder(input, { numChannels: 1 })
 
-		//start the recording process
-		rec.record()
+    //start the recording process
+    rec.record()
 
-		console.log("Recording started");
+    console.log("Recording started");
 
-	}).catch(function(err) {
-	  	//enable the record button if getUserMedia() fails
-    	recordButton.disabled = false;
-    	stopButton.disabled = true;
-    	pauseButton.disabled = true
-	});
+  }).catch(function (err) {
+    //enable the record button if getUserMedia() fails
+    recordButton.disabled = false;
+    stopButton.disabled = true;
+    pauseButton.disabled = true
+  });
 }
 
-function pauseRecording(){
-	console.log("pauseButton clicked rec.recording=",rec.recording );
-	if (rec.recording){
-		//pause
-		rec.stop();
-		pauseButton.innerHTML="Resume";
-	}else{
-		//resume
-		rec.record()
-		pauseButton.innerHTML="Pause";
+function pauseRecording() {
+  console.log("pauseButton clicked rec.recording=", rec.recording);
+  if (rec.recording) {
+    //pause
+    rec.stop();
+    pauseButton.innerHTML = "Resume";
+  } else {
+    //resume
+    rec.record()
+    pauseButton.innerHTML = "Pause";
 
-	}
+  }
 }
 
 function stopRecording() {
-	console.log("stopButton clicked");
+  console.log("stopButton clicked");
 
-	//disable the stop button, enable the record too allow for new recordings
-	stopButton.disabled = true;
-	recordButton.disabled = false;
-	pauseButton.disabled = true;
+  //disable the stop button, enable the record too allow for new recordings
+  stopButton.disabled = true;
+  recordButton.disabled = false;
+  pauseButton.disabled = true;
 
-	//reset button just in case the recording is stopped while paused
-	pauseButton.innerHTML="Pause";
-	
-	//tell the recorder to stop the recording
-	rec.stop();
+  //reset button just in case the recording is stopped while paused
+  pauseButton.innerHTML = "Pause";
 
-	//stop microphone access
-	gumStream.getAudioTracks()[0].stop();
+  //tell the recorder to stop the recording
+  rec.stop();
 
-	//create the wav blob and pass it on to createDownloadLink
-	rec.exportWAV(createDownloadLink);
+  //stop microphone access
+  gumStream.getAudioTracks()[0].stop();
+
+  //create the wav blob and pass it on to createDownloadLink
+  rec.exportWAV(createDownloadLink);
 }
 
+var voiceNotes = 0;
+
 function createDownloadLink(blob) {
-	
-	var url = URL.createObjectURL(blob);
-	var au = document.createElement('audio');
-	var li = document.createElement('li');
-	var link = document.createElement('a');
 
-	//name of .wav file to use during upload and download (without extendion)
-	var filename = new Date().toISOString();
+  var url = URL.createObjectURL(blob);
+  var au = document.createElement('audio');
+  var li = document.createElement('li');
+  var link = document.createElement('a');
+  var input = document.createElement('input');
+  input.setAttribute("type", "hidden");
+  input.setAttribute("id", "voiceNote" + voiceNotes);
+  input.setAttribute("name", "voiceNote" + voiceNotes);
 
-	//add controls to the <audio> element
-	au.controls = true;
-	au.src = url;
+  //name of .wav file to use during upload and download (without extendion)
+  var filename = "Comentario" + voiceNotes;
 
-	//save to disk link
-	link.href = url;
-	link.download = filename+".wav"; //download forces the browser to donwload the file using the  filename
-	link.innerHTML = "Save to disk";
+  //add controls to the <audio> element
+  au.controls = true;
+  au.src = url;
 
-	//add the new audio element to li
-	li.appendChild(au);
-	
-	//add the filename to the li
-	li.appendChild(document.createTextNode(filename+".wav "))
+  //save to disk link
+  link.href = url;
+  link.download = filename + ".wav"; //download forces the browser to donwload the file using the  filename
+  link.innerHTML = "Save to disk";
 
-	//add the save to disk link to li
-	li.appendChild(link);
-	
-	//upload link
-	var upload = document.createElement('a');
-	upload.href="#";
-	upload.innerHTML = "Upload";
-	upload.addEventListener("click", function(event){
-		  var xhr=new XMLHttpRequest();
-		  xhr.onload=function(e) {
-		      if(this.readyState === 4) {
-		          console.log("Server returned: ",e.target.responseText);
-		      }
-		  };
-		  var fd=new FormData();
-		  fd.append("audio_data",blob, filename);
-		  xhr.open("POST","upload.php",true);
-		  xhr.send(fd);
-	})
-	li.appendChild(document.createTextNode (" "))//add a space in between
-	li.appendChild(upload)//add the upload link to li
+  //add the hidden input to send audio through mail to li
+  li.appendChild(input);
 
-	//add the li element to the ol
-	recordingsList.appendChild(li);
+  //add the new audio element to li
+  li.appendChild(au);
+
+  //add the filename to the li
+  li.appendChild(document.createTextNode(filename + ".wav "))
+
+  //add the save to disk link to li
+  li.appendChild(link);
+
+  //upload link
+  var upload = document.createElement('a');
+  upload.href = "#";
+  upload.innerHTML = "Upload";
+  upload.addEventListener("click", function (event) {
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function (e) {
+      if (this.readyState === 4) {
+        console.log("Server returned: ", e.target.responseText);
+      }
+    };
+    var fd = new FormData();
+    fd.append("audio_data", blob, filename);
+    xhr.open("POST", "upload.php", true);
+    xhr.send(fd);
+  })
+  li.appendChild(document.createTextNode(" "))//add a space in between
+  li.appendChild(upload)//add the upload link to li
+
+  //add the li element to the ol
+  recordingsList.appendChild(li);
+  recordingsList.appendChild(input);
+  //recordingsList.appendChild(input.attr('type', 'hidden').attr('id', "recordN" + voiceNotes + 1));
+  $("#voiceNote" + voiceNotes).val(url);
+  voiceNotes = parseInt(voiceNotes, 10) + 1;
 }
 
 
 
 //
 var SpeechRecognition = window.webkitSpeechRecognition;
-      
+
 var recognition = new SpeechRecognition();
- 
+
 var Textbox = $('#textbox');
 var instructions = $('instructions');
- 
+
 var Content = '';
- 
+
 recognition.continuous = true;
- 
-recognition.onresult = function(event) {
- 
+
+recognition.onresult = function (event) {
+
   var current = event.resultIndex;
- 
+
   var transcript = event.results[current][0].transcript;
- 
-    Content += transcript;
-    Textbox.val(Content);
-  
+
+  Content += transcript;
+  Textbox.val(Content);
+
 };
- 
-recognition.onstart = function() { 
+
+recognition.onstart = function () {
   instructions.text('Voice recognition is ON.');
 }
- 
-recognition.onspeechend = function() {
+
+recognition.onspeechend = function () {
   instructions.text('No activity.');
 }
- 
-recognition.onerror = function(event) {
-  if(event.error == 'no-speech') {
-    instructions.text('Try again.');  
+
+recognition.onerror = function (event) {
+  if (event.error == 'no-speech') {
+    instructions.text('Try again.');
   }
 }
 
- 
-$('#start-btn').on('click', function(e) {
+
+$('#start-btn').on('click', function (e) {
   if (Content.length) {
     Content += ' ';
   }
   recognition.start();
 });
 
-$('#stop-btn').on('click', function(e) {
+$('#stop-btn').on('click', function (e) {
   if (Content.length) {
     Content += ' ';
   }
   recognition.stop();
 });
- 
-Textbox.on('input', function() {
+
+Textbox.on('input', function () {
   Content = $(this).val();
 })
